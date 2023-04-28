7c6fbc03724ee44f832396934e5a69add874633e
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-04-28 08:08:56 +0500

    2

--- tasks.py
+++ tasks.py
@@ -1,4 +1,18 @@
+
 '''
+# 20. Valid Parentheses (Допустимые  скобки)
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Дана строка строки, содержащая только символы '(', ')', '{', '}', '[' и ']', определите, является ли входная строка допустимой.
+Входная строка допустима, если:
+Открытые скобки должны быть закрыты скобками того же типа.
+Открытые скобки должны быть закрыты в правильном порядке.
+Каждая закрывающая скобка имеет соответствующую открытую скобку того же типа.
+
 Example 1:
 Input: s = "()"
 Output: true
@@ -29,3 +43,4 @@ class Solution(object):
 
 obj1 = Solution()
 print(obj1.isValid('(])'))
+
