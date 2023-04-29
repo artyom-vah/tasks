2490560790ca27637dcd7eb982cfd2ab14288e75
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-04-29 20:04:02 +0500

    4

--- tasks.py
+++ tasks.py
@@ -39,7 +39,7 @@ target = 6
 obj = Solution()
 print(obj.twoSum(nums, target))
 
-##***********************************************************************************************************************
+#***********************************************************************************************************************
 '''
 9. Palindrome Number
 Given an integer x, return true if x is a palindrome, and false otherwise.
@@ -67,6 +67,101 @@ class Solution(object):
 
 obj = Solution()
 print(obj.isPalindrome(1221))
+
+# ***********************************************************************************************************************
+'''
+13. Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II.
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+Example 1:
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+'''
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        total = 0
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        return total + roman[s[-1]]
+
+obj = Solution()
+print(obj.romanToInt('XVII')) # 17
+
+# ***********************************************************************************************************************
+'''
+14. Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+'''
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if len(strs) == 0:
+            return ""
+
+        base = strs[0]
+        for i in range(len(base)):
+            for word in strs[1:]:
+                if i == len(word) or word[i] != base[i]:
+                    return base[0:i]
+        return base
+
+
+strs = ["flower", "flow", "flight"]
+obj = Solution()
+print(obj.longestCommonPrefix(strs))
 
 # ***********************************************************************************************************************
 '''
@@ -114,4 +209,6 @@ class Solution(object):
 obj = Solution()
 print(obj.isValid('(])'))  # False
 print(obj.isValid('()[]{}'))  # True
-# ***********************************************************************************************************************
+
+#***********************************************************************************************************************
+#***********************************************************************************************************************
