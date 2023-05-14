cd89c01a05b89dc522095119ca6cd6943d80e70a
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-05-14 09:59:46 +0500

    101

--- leetcode.py
+++ leetcode.py
@@ -812,6 +812,13 @@ print(result)
 18) 101. Symmetric Tree (Симметричное дерево)
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 Учитывая корень бинарного дерева, проверьте, является ли он зеркалом самого себя (т. Е. Симметричным относительно своего центра).
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
 '''
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
