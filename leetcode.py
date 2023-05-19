c7b077fe4d1d39e95ac94d08f42302022de9f92b
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-05-19 11:42:33 +0500

    leetcode-160

--- leetcode.py
+++ leetcode.py
@@ -30,6 +30,7 @@
 28) 141. Linked List Cycle (Цикл связанного списка)
 29) 144. Binary Tree Preorder Traversal (Обход предварительного порядка двоичного дерева)
 30) 145. Binary Tree Postorder Traversal (Обход почтового заказа двоичного дерева)
+31) 160. Intersection of Two Linked Lists (Пересечение двух связанных списков)
 
 
 
@@ -1538,6 +1539,104 @@ result = solution.postorderTraversal(tree)
 
 # Вывод результата в консоль
 print(result)
+
+# ***********************************************************************************************************************
+
+'''
+160. Intersection of Two Linked Lists (Пересечение двух связанных списков)
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+For example, the following two linked lists begin to intersect at node c1:
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+Note that the linked lists must retain their original structure after the function returns.
+Custom Judge:
+The inputs to the judge are given as follows (your program is not given these inputs):
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+
+Example 1:
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+- Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+
+Example 2:
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+Example 3:
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+'''
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        first_set = set()
+        curr = headA
+
+        while curr:
+            first_set.add(curr)
+            curr = curr.next
+
+        curr = headB
+        while curr:
+            if curr in first_set:
+                return curr
+            curr = curr.next
+
+        return None
+
+
+# Создание списка A: 1 -> 2 -> 3 -> 4 -> 5
+listA = ListNode(1)
+listA.next = ListNode(2)
+listA.next.next = ListNode(3)
+listA.next.next.next = ListNode(4)
+listA.next.next.next.next = ListNode(5)
+
+# Создание списка B: 6 -> 7 -> 8
+listB = ListNode(6)
+listB.next = ListNode(7)
+listB.next.next = ListNode(8)
+
+# Общая часть списков: 9 -> 10
+intersection = ListNode(9)
+intersection.next = ListNode(10)
+
+# Установка пересечения списков A и B
+listA.next.next.next.next.next = intersection
+listB.next.next.next = intersection
+
+# Создание объекта класса Solution
+solution = Solution()
+
+# Вызов метода getIntersectionNode
+result = solution.getIntersectionNode(listA, listB)
+
+# Вывод результата в консоль
+if result:
+    print(result.val)
+else:
+    print("No intersection")
+
+# ***********************************************************************************************************************
+
 
 # ***********************************************************************************************************************
 # ***********************************************************************************************************************
