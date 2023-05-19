38bb2a03362f04464f03f4f30dd939ac0327b212
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-05-19 11:50:18 +0500

    codewars-7-7-1

--- codewars.py
+++ codewars.py
@@ -305,7 +305,42 @@ print(accum("abcd"))
 '''
 (7 kyu)
 7) You're a square! (Ты квадрат!)
+A square of squares
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+Task
+Given an integral number, determine if it's a square number:
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
 '''
+from math import sqrt
+
+
+def is_square(n):
+    # return n >= 0 and (n ** 0.5) % 1 == 0 # тоже все верно
+    # return n>=0 and sqrt(n).is_integer() # тоже все верно
+    if n == 0:
+        return True
+    elif n > 0:
+        if n == sqrt(n) ** 2:
+            return True
+        return False
+    return False
+
+
+print(is_square(16))  # True
+print(is_square(4))  # True
+print(is_square(25))  # True
+print(is_square(26))  # False
+
 
 # ***********************************************************************************************************************
 ### *** (6 kyu) *** ###
