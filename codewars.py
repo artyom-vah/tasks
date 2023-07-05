2d433d4214c0600a7bac692f3887c5b85a8f41b6
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-07-05 12:15:31 +0500

    codewars-7-15

--- codewars.py
+++ codewars.py
@@ -24,6 +24,8 @@
     12) Shortest Word (Самое короткое слово)
     13) Complementary DNA (Комплементарная ДНК)
     14) Credit Card Mask (Маска кредитной карты)
+    15) Sum of two lowest positive integers (Сумма двух наименьших положительных целых чисел)
+
 
 
 
@@ -550,8 +552,24 @@ def maskify(cc):
 
 print(maskify("4556364607935616"))
 # ***********************************************************************************************************************
+'''
+(7 kyu)
+    15) Sum of two lowest positive integers (Сумма двух наименьших положительных целых чисел)
+    
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
+ No floats or non-positive integers will be passed.
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+'''
 
 
+def sum_two_smallest_numbers(numbers):
+    # your code here
+    numbers.sort()
+    return numbers[0] + numbers[1]
+
+
+print(sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]))
 # ***********************************************************************************************************************
 
 
