539a47ee06fca2ca5f0f76a32526b68bb3b28f78
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-07-05 14:55:04 +0500

    codewars-6-8

--- codewars.py
+++ codewars.py
@@ -37,6 +37,8 @@
     5) Who likes it?
     6) Array.diff
     7) Bit Counting (Подсчет битов)
+    8) Find The Parity Outlier
+
 
 
 
@@ -879,7 +881,34 @@ print(funk(1234))  # 5
 
 #
 # ***********************************************************************************************************************
+'''
+(6 kyu)
 
+8) Find The Parity Outlier
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array 
+is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+Вам предоставляется массив (который будет иметь длину не менее 3, но может быть очень большим), содержащий целые числа. 
+Массив либо полностью состоит из нечетных целых чисел, либо полностью состоит из четных целых чисел, за исключением 
+единственного целого числа N. Напишите метод, который принимает массив в качестве аргумента и возвращает этот "выброс" N.
+
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+'''
+def find_out(integers: list) -> int:
+    l1 = [i for i in integers if i % 2 == 0]
+    l2 = [i for i in integers if i % 2 == 1]
+    if len(l1) < len(l2):
+        return l1[0]
+    return l2[0]
+
+
+print(find_out([2, 4, 0, 100, 4, 11, 2602, 36])) # 11
+print(find_out([160, 3, 1719, 19, 11, 13, -21])) # 160
 
 # ***********************************************************************************************************************
 
