149cdc678b11e2cf0dea57a47ac0d47fadbb2bee
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-08-01 20:22:14 +0500

    add codewars 5-9

--- codewars.py
+++ codewars.py
@@ -68,7 +68,7 @@
     6) Valid Parentheses (Допустимые скобки)
     7) Directions Reduction (Направления Сокращение)
     8) Calculating with Functions (Расчет с функциями)
-
+    9) Rot13
 
 '''
 # ***********************************************************************************************************************
@@ -1509,6 +1509,40 @@ def divided_by(i): return lambda x: x//i
 print(seven(times(five())))  # 35
 print(four(plus(three())))  # 7
 print(eight(minus(three())))  # 5
+# ***********************************************************************************************************************
+
+'''
+(5 kyu)
+9) Rot13
+
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
+ROT13 is an example of the Caesar cipher. Create a function that takes a string and returns the string ciphered with Rot13. 
+If there are numbers or special characters included in the string, they should be returned as they are. Only letters 
+from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+Please note that using encode is considered cheating.
+
+ROT13 - это простой шифр подстановки букв, который заменяет букву на 13 букв после нее в алфавите.
+ROT13 - это пример шифра Цезаря. Создайте функцию, которая принимает строку и возвращает строку, зашифрованную с помощью Rot13.
+Если в строку включены цифры или специальные символы, они должны быть возвращены в том виде, в каком они есть. Только буквы
+из латинского/английского алфавита должны быть сдвинуты, как в оригинальной "реализации" Rot13.
+Пожалуйста, обратите внимание, что использование encode считается мошенничеством.
+
+'''
+def rot13(message):
+    res = ''
+    for i in message:
+        if (ord(i) >= ord('A') and ord(i) <= ord('M')) or (ord(i) >= ord('a') and ord(i) <= ord('m')):
+            res += chr(ord(i) + 13)
+        elif (ord(i) >= ord('N') and ord(i) <= ord('Z')) or (ord(i) >= ord('n') and ord(i) <= ord('z')):
+            res += chr(ord(i) - 13)
+        else:
+            res += i
+    return res
+
+
+print(rot13("test"))  # grfg
+print(rot13("Test"))  # Grfg
+print(rot13("aA bB zZ 1234 *!?%"))  # nN oO mM 1234 *!?%
 
 # ***********************************************************************************************************************
 # ***********************************************************************************************************************
